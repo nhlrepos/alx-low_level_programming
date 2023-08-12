7c332fb96b31b03ebe895be046dd2e9856f677a8
nhlrepos <113945693+nhlrepos@users.noreply.github.com>
2023-08-12 18:53:36 +0200

    Update 3-print_alphabets.c betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	char alpha[52]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for(i=0;i<52;i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alpha[i]);
 	}
