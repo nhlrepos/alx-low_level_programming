ac1c1f22329dcb7dd3ba92940ff75a00a3aa1285
nhlrepos <113945693+nhlrepos@users.noreply.github.com>
2023-08-12 19:20:15 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,26 @@
  */
 int main(void)
 {
-        int i, j;
-        int c = 48;
-        
-        for (i = 48; i < 58; i++)
-        {
-                for (j = c; j < 58; j++)
-                {
-                        if (i != j)
-                        {
-                                putchar(i);
-                                putchar(j);
-                                if (i != 56 || j != 57)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
+	int n, m;
+        int c =49;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = c; m < 58; m++)
+		{
+			if (m != n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
                 c++;
-        }
-        
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
