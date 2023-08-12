62e7fd12dd8e1aaffae18e4d371e2ae4349fd4cf
nhlrepos <113945693+nhlrepos@users.noreply.github.com>
2023-08-12 20:01:16 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,34 +8,28 @@
  */
 int main(void)
 {
-        int i, j, k, l ;
+	int i, j;
 
-        for(i=48;i<58;i++)
-        {
-            for(j=48;j<58;j++)
-            {   
-                for(k=48;k<58;k++)
-                { 
-                    for(l=48;l<58;l++)
-                    { 
-                            if (i<=k && j<l)
-                            {
-                                putchar(i);
-                                putchar(j);
-                                putchar(' ');
-                                putchar(k);
-                                putchar(l);
-                                if (j!=56 || k!= 57)
-                                {
-                                putchar(',');
-                                putchar(' ');
-                                }
-                            }
-                    }
-                }
-            }
-        }
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
